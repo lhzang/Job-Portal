93784f7ff0ae7d0150f3bcdeceb8ea508e1e3672
lhzang <gianglh4973@gmail.com>
2025-04-24 07:25:50 +0700

    recruiter

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+import { jobsData } from "../assets/assets";
 
 export const AppContext = createContext();
 
@@ -10,10 +11,28 @@ export const AppContextProvider = (props) => {
 
   const [isSearched, setIsSearched] = useState(false);
 
+  const [jobs, setJobs] = useState([]);
+
+  const [showRecruiterLogin, setShowRecruiterLogin] = useState(false);
+
+  // Function to fetch jobs
+  const fetchJobs = async () => {
+    setJobs(jobsData);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
   const value = {
     setSearchFilter,
     searchFilter,
-    isSearched, setIsSearched,
+    isSearched,
+    setIsSearched,
+    jobs,
+    setJobs,
+    showRecruiterLogin,
+    setShowRecruiterLogin,
   };
 
   return (
